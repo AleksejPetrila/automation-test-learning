552be5465bc116e867c29a9d95c4cd18b02f8ebf
Aleksejus Petrila <aleksejus.petrila@gmail.com>
2026-03-01 12:47:07 +0100

    changed index page

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -8,14 +8,23 @@ from ui.pages.login_page import LoginPage
 def test_login_success(page, demo_server_url):
     login_page = LoginPage(page)
     login_page.open(demo_server_url)
-    login_page.login("admin", "secret")
+    login_page.login("admin", "secret", "2")
     login_page.expect_message("Login successful")
 
 
 @pytest.mark.ui
 @pytest.mark.regression
-def test_login_invalid_credentials(page, demo_server_url):
+def test_login_invalid_credentials_two(page, demo_server_url):
     login_page = LoginPage(page)
     login_page.open(demo_server_url)
-    login_page.login("wrong", "wrong")
+    login_page.login("wrong", "wrong", "2")
+    login_page.expect_message("Invalid credentials")
+
+
+@pytest.mark.ui
+@pytest.mark.regression
+def test_login_wrong_age_format(page, demo_server_url):
+    login_page = LoginPage(page)
+    login_page.open(demo_server_url)
+    login_page.login("admin", "secret", "abc")
     login_page.expect_message("Invalid credentials")
--- ui/pages/login_page.py
+++ ui/pages/login_page.py
@@ -1,20 +1,22 @@
-class LoginPage:
+from playwright.sync_api import expect
 
+class LoginPage:
     def __init__(self, page):
         self.page = page
-        self.username_input = page.locator("#user-input")
-        self.password_input = page.locator("#password")
-        self.login_button = page.locator("#user-input-btn")
-        self.message = page.locator("#message")
+        self.username_input = page.get_by_test_id("username-input")
+        self.password_input = page.get_by_test_id("password-input")
+        self.age_input = page.get_by_test_id("age-input")
+        self.login_button = page.get_by_test_id("login-button")
+        self.message = page.get_by_test_id("message")
 
     def open(self, base_url):
         self.page.goto(base_url)
 
-    def login(self, username, password):
+    def login(self, username, password, age):
         self.username_input.fill(username)
         self.password_input.fill(password)
+        self.age_input.fill(age)
         self.login_button.click()
 
     def expect_message(self, expected_text):
-        from playwright.sync_api import expect
         expect(self.message).to_have_text(expected_text)
